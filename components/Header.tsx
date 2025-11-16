f38a117121503b0402e0c1c6298b448414a93d11
alineguedes51 <alineseve22@gmail.com>
2025-11-16 15:25:50 -0300

    Update Header.tsx

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,7 +1,6 @@
 // components/Header.tsx
 
 import React from 'react';
-// 1. ADICIONA ESTA LINHA: Importa a imagem como uma variável
 import logoImage from './logo.png'; 
 
 export const Header: React.FC = () => {
@@ -10,11 +9,10 @@ export const Header: React.FC = () => {
             <div className="container mx-auto px-4 py-3">
                 <a href="/" className="flex flex-col md:flex-row items-center justify-center no-underline" aria-label="Página Inicial">
                     <img
-                        // 2. ALTERA ESTA LINHA: 
-                        // Muda de src="/logo.png" para src={logoImage}
                         src={logoImage}
                         alt="Logo Gestão Sustentável Manguezais de Igarassu"
-                        className="h-14 w-auto mb-2 md:mb-0 md:mr-4"
+                        // ALTERAÇÃO AQUI: Mudei de h-14 para h-20
+                        className="h-20 w-auto mb-2 md:mb-0 md:mr-4"
                     />
                     <h1 className="text-lg sm:text-xl md:text-2xl font-bold text-earthy-brown text-center md:text-left">
                         Gestão Sustentável dos Manguezais de Igarassu
